83040b2ca602bc15a00a8aa83ec394562674d76f
rozandoz <powerstatus524@gmail.com>
2024-05-05 17:13:18 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ config.window_decorations = 'TITLE|RESIZE'
 config.hide_tab_bar_if_only_one_tab = true;
 config.window_frame = { font_size = 13.0 }
 config.color_scheme = 'OneDark (base16)'
+config.enable_scroll_bar = true
 
 -- fonts
 config.font = wezterm.font('JetBrains Mono')
@@ -18,37 +19,29 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 end
 
 -- binds
-config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.keys = {
-    {
-        key = 'h',
-        mods = 'LEADER',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-        key = 'v',
-        mods = 'LEADER',
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    { key = 't', mods = 'LEADER', action = wezterm.action.ShowTabNavigator },
+
+    -- panes
+    { key = 's', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'v', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'q', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
+
+    -- clipboard
     { key = 'v', mods = 'CTRL',   action = wezterm.action.PasteFrom 'Clipboard' },
+    { key = 'c', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+
+    -- tabs
+    { key = 't', mods = 'LEADER', action = wezterm.action.ShowTabNavigator },
+    { key = 'o', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    { key = 'x', mods = 'LEADER', action = wezterm.action.CloseCurrentTab { confirm = true } },
+    { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
+    { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
 }
 
 -- plugins
 local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
-smart_splits.apply_to_config(config, {
-  -- the default config is here, if you'd like to use the default keys,
-  -- you can omit this configuration table parameter and just use
-  -- smart_splits.apply_to_config(config)
-
-  -- directional keys to use in order of: left, down, up, right
-  direction_keys = { 'h', 'j', 'k', 'l' },
-  -- modifier keys to combine with direction_keys
-  modifiers = {
-    move = 'CTRL', -- modifier to use for pane movement, e.g. CTRL+h to move left
-    resize = 'META', -- modifier to use for pane resize, e.g. META+h to resize to the left
-  },
-})
+smart_splits.apply_to_config(config)
 
 return config
